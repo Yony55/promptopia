6be65a4378ac3cbde6496e3fce064badea00e168
Yony55 <yony_ms@live.com>
2023-06-09 16:00:42 -0600

    Create Prompt

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,58 @@
+// Page component which contains a form to create a prompt
+"use client"
+
+// Import hooks
+import { useState } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+
+// Import components
+import Form from "@components/Form"
+
+const CreatePrompt = () => {
+  const router = useRouter(); 
+  const { data: session } = useSession(); 
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+// API call to create a prompt
+const createPrompt = async (e) => {
+  e.preventDefault();
+  setSubmitting(true);
+
+  try {
+    const response = await fetch('api/prompt/new',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        prompt: post.prompt,
+        userId: session?.user.id,
+        tag: post.tag,
+      }),
+    })
+    if(response.ok) {
+      router.push('/');
+    }
+  } catch (error) {
+    console.log(error);
+  } finally{ 
+     setSubmitting(false);
+  }
+}
+
+  return (
+    // Pass in props to Form component
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  )
+}
+
+export default CreatePrompt
