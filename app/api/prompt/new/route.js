fa439eca90df3dfdd9963792f94dd2639da60014
Yony55 <yony_ms@live.com>
2023-06-09 16:00:42 -0600

    Create Prompt

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,21 @@
+import { connectToDatabase } from "@utils/database";
+import Prompt from "@models/prompt";
+
+// API route for creating a new prompt
+export const POST = async (req) => {
+  const { userId, prompt, tag } = await req.json();
+  try{
+    // Se conecta a la base de datos
+    await connectToDatabase(); 
+    
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt, 
+      tag
+    })
+    await newPrompt.save();
+    return new Response(JSON.stringify(newPrompt), {status: 201}); 
+  } catch(error){
+    return new Response("Error creating prompt", {status: 500});
+  }
+};
