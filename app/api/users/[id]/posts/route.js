1e5a74b56abf1298f2b66348318dbb7bf48098a2
Yony55 <yony_ms@live.com>
2023-06-23 15:47:39 -0600

    Cache fix

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -7,7 +7,8 @@ export const GET = async (req, { params }) => {
     const prompts = await Prompt.find({ creator: params.id }).populate(
       "creator"
     );
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return new Response(JSON.stringify(prompts), { status: 200 }, { "Cache-Control":
+    "no-cache, no-store, max-age=0, must-revalidate"});
   } catch (error) {
     return new Response("Failed to fetch.", { status: 500 });
   }
