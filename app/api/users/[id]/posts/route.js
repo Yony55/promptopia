c61684f343388d02902eb27b903b2dd2362caadc
Yony55 <yony_ms@live.com>
2023-06-23 18:18:31 -0600

    No cache on prompts fetch

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,14 +1,19 @@
 import { connectToDatabase } from "@utils/database";
 import Prompt from "@models/prompt";
+import { headers } from 'next/headers';
 
 export const GET = async (req, { params }) => {
+  const headersList = headers();
+  const referer = headersList.get('referer');
   try {
     await connectToDatabase();
     const prompts = await Prompt.find({ creator: params.id }).populate(
       "creator"
     );
-    return new Response(JSON.stringify(prompts), { status: 200 }, { "Cache-Control":
-    "no-cache, no-store, max-age=0, must-revalidate"});
+    return new Response(JSON.stringify(prompts), { status: 200, headers: {
+      referer: referer,
+      'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
+    } });
   } catch (error) {
     return new Response("Failed to fetch.", { status: 500 });
   }
