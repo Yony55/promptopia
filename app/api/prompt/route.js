70f9e5b891ee15c63e80f4438ec4e2abcaf25014
Yony55 <yony_ms@live.com>
2023-06-23 18:34:54 -0600

    Cache fix

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,12 +1,21 @@
 import { connectToDatabase } from "@utils/database";
 import Prompt from "@models/prompt";
+import { headers } from "next/headers";
 
 //API route for fetching all prompts
 export const GET = async (req) => {
+  const headersList = headers();
+  const referer = headersList.get("referer");
   try {
     await connectToDatabase();
     const prompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+      headers: {
+        referer: referer,
+        "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
+      },
+    });
   } catch (error) {
     return new Response("Failed to fetch.", { status: 500 });
   }
